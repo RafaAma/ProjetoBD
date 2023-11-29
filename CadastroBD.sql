730fbb3b857e41f5252f57ba0647201bf872dec9
RafaAma <152069468+RafaAma@users.noreply.github.com>
2023-11-28 22:03:11 -0300

    Add files via upload

---
+++ CadastroBD.sql
@@ -0,0 +1,48 @@
+--
+-- Banco de dados: `cadastro`
+--
+CREATE DATABASE IF NOT EXISTS `cadastro` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cadastro`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `data_nasc` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_nasc`) VALUES
+(1, 'Rafael do Amaral Correia', 'rafael.amaralcorreia1@gmail.com', '202cb962ac59075b964b07152d234b70', '2006-09-01'),
+(13, 'Nickolas', 'Nickolas@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', '2006-08-01');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
